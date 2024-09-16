3f4ef2be1131c66d6532556495c6ee46982420d2
kauu <133917142+yue845@users.noreply.github.com>
2024-09-16 10:49:20 -0300

    k

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-alert ("você clicou no botão");
-document.querySelector ("button");
-
+document.querySelector ('button');
+const botao = (button);
+botao.onclick =
+function {
+    alert ("você clicou no botão");
+}
