a6c70589c6c602fd9e90d0fb17ff3739dedca835
kauu <133917142+yue845@users.noreply.github.com>
2024-09-27 08:53:46 -0300

    bn g

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ botao.onclick = function () {
 }
 
 function soma(num1, num2) {
-    return num1 + num2
+    return num1 + num2;
 }
 console.log(soma(5,3));
+
+function areaQuadrado(lado){
+    return lado**2;
+}
+console.log(areaQuadrado(6));
