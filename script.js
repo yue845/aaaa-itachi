2e44b6d6412803154804408e43200482bfeecbb4
kauu <133917142+yue845@users.noreply.github.com>
2024-09-27 08:59:28 -0300

    g

--- script.js
+++ script.js
@@ -13,3 +13,7 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log(areaQuadrado(6));
+
+let media = function(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3/3;
+}
